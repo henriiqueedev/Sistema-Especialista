2aec0fbbde8c2fde38a8584d1376e440fdc10b74
Henrique <henriquepg1011@gmail.com>
2025-10-01 23:48:35 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/sistemaespecialista/sistemaespecialista/SistemaespecialistaApplication.java
@@ -0,0 +1,13 @@
+package com.sistemaespecialista.sistemaespecialista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaespecialistaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaespecialistaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sistemaespecialista/sistemaespecialista/SistemaespecialistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemaespecialista.sistemaespecialista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaespecialistaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
