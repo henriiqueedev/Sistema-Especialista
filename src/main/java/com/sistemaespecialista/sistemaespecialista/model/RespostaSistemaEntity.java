ca2dd879772d3e9e6d9392817a89bb53f971ded9
Henrique <henriquepg1011@gmail.com>
2025-11-03 22:05:14 -0300

    Criando nova entidade

---
+++ src/main/java/com/sistemaespecialista/sistemaespecialista/model/RespostaSistemaEntity.java
@@ -0,0 +1,27 @@
+package com.sistemaespecialista.sistemaespecialista.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "respostas_sistema")
+public class RespostaSistemaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "usuario_id")
+    private UserEntity usuario;
+
+    private float percentualGastosFixos;
+    private float percentualInvestimentos;
+    private float percentualQuitacaoDividas;
+    private float percentualReservaEmergencia;
+    private float percentualObjetivo;
+}
--- src/main/java/com/sistemaespecialista/sistemaespecialista/model/UserEntity.java
+++ src/main/java/com/sistemaespecialista/sistemaespecialista/model/UserEntity.java
@@ -1,40 +1,26 @@
 package com.sistemaespecialista.sistemaespecialista.model;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
 import lombok.*;
-
-
-import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "usuarios")
 @Entity
+@Table(name = "usuarios")
 public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull
     private float salario;
+    private float gastoFixo;
 
-    @NotNull
     private String perfil;
-
     private int filhos;
-
     private float divida;
-    
     private float reserva;
-
     private String objetivo;
-
-    @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
-    private List<RespostaEntity> respostas;
-
-
 }
