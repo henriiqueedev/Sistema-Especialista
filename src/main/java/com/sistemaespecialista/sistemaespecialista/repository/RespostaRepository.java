dfb4558bd2adae13d412e7796dd25e5ce4b7889c
Henrique <henriquepg1011@gmail.com>
2025-11-05 00:04:30 -0300

    Ajustando RespostaRepository.java

--- src/main/java/com/sistemaespecialista/sistemaespecialista/repository/RespostaRepository.java
+++ src/main/java/com/sistemaespecialista/sistemaespecialista/repository/RespostaRepository.java
@@ -1,7 +1,7 @@
 package com.sistemaespecialista.sistemaespecialista.repository;
 
-import com.sistemaespecialista.sistemaespecialista.entities.PerguntaEntity;
+import com.sistemaespecialista.sistemaespecialista.entities.RespostaEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RespostaRepository extends JpaRepository <PerguntaEntity, Long> {
+public interface RespostaRepository extends JpaRepository <RespostaEntity, Long> {
 }
